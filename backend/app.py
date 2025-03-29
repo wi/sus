1bf9741956db333b6a99b9f9072be8a85085489f
Rumi Khamidov <rukham004@gmail.com>
2025-03-29 13:08:57 -0400

    test app

--- backend/app.py
+++ backend/app.py
@@ -3,5 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def yo_world():
+    return "<p>Yo, World!</p>"
+
+@app.route("/world")
 def hello_world():
     return "<p>Hello, World!</p>"
